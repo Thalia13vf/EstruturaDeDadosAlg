19b9c102d5d386983b35bd0ca6d464fd7d7370e2
Thalia13vf <thalia13vf@gmail.com>
2020-05-29 23:55:43 -0300

    Lista Ligada - LinkedList(API Java)

---
+++ src/com/estruturadedadosalg/listasligadas/testes/TesteLinkedList.java
@@ -0,0 +1,27 @@
+package com.estruturadedadosalg.listasligadas.testes;
+
+import java.util.LinkedList;
+
+public class TesteLinkedList {
+	public static void main(String[] args) {
+		Aluno maria = new Aluno();
+		maria.setNome("Maria");
+		
+		Aluno manoel = new Aluno();
+		manoel.setNome("Manoel");
+		
+		Aluno joaquim = new Aluno();
+		joaquim.setNome("Joaquim");
+		
+		LinkedList<Aluno> listaLigada = new LinkedList<>();
+		listaLigada.add(maria);
+		listaLigada.add(manoel);
+		listaLigada.add(1, joaquim);
+		
+		for(int i = 0; i < listaLigada.size(); i++) {
+			System.out.println(listaLigada.get(i));
+		}
+		
+		System.out.println(listaLigada);
+	}
+}
